28b5a93368add1425aee4ba2282805f8659b1e72
SztanyiGy <gyuriii2002@gmail.com>
2026-03-13 10:31:23 +0100

    visszafejtes

---
+++ playfair.h
@@ -0,0 +1,10 @@
+#ifndef PLAYFAIR_H
+#define PLAYFAIR_H
+
+void generateMatrix(char *key);
+void prepareText(char *input, char *output);
+void encryptText(char *text);
+void decryptText(char *text);
+void printMatrix();
+
+#endif
